b18b669a0c85aba0bca9a0ee3566b3abd2604529
arturbmallmann <arturbmallmann@gmail.com>
2016-05-19 04:28:21 +0000

    funcionando insercao

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "RN.h"
+
+int main(int argc, char *argv[])
+{
+  RNtree* arv = NULL;
+  /*
+  arv = Insere(arv,10);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,20);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,5);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,50);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,60);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,70);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+
+if(Consulta(40,arv)) printf("O Nodo 40 esta na arvore\n");
+  else printf("O Nodo 40 nao esta na arvore\n");
+  if(Consulta(10,arv)) printf("O Nodo 10 esta na arvore\n");
+  else printf("O Nodo 10 nao esta na arvore\n");
+  arv = Remove(arv, 5);
+  Desenha(arv,0);
+  arv = Remove(arv, 40);
+  Desenha(arv,0);
+ // Destroi(arv);
+  //*/
+  //*
+  arv = Insere(arv,13);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,9);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,14);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,15);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,3);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,4);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,8);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,10);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  arv = Insere(arv,11);
+  Desenha(arv,0);
+ 	putc('\n',stdout);
+  return 0;
+  //*/
+}
